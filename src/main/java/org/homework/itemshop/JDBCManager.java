a04490039f5997b70c00f0e908ba47c38782547e
alishka2000 <vj_alishka@mail.ru>
2021-03-04 02:43:46 +0600

    MainMenu
    Create MainMenu Optional

--- src/main/java/org/homework/itemshop/JDBCManager.java
+++ src/main/java/org/homework/itemshop/JDBCManager.java
@@ -6,11 +6,19 @@ import org.homework.itemshop.service.DbServiceImpl;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 public class JDBCManager {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        DbService service = new DbServiceImpl();
+    static int choose;
+    static Scanner keyboard = new Scanner(System.in);
+    private static final DbService service = new DbServiceImpl();
+    private static final ItemShop item = new ItemShop();
 
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        //DbService service = new DbServiceImpl();
+
+
+        MainMenu();
         /*ItemShop item = new ItemShop();
         item.setName("Milk");
         item.setPrice(390.2);
@@ -27,11 +35,60 @@ public class JDBCManager {
         for (ItemShop temp: items) {
             System.out.println(temp.toString());
         }*/
-        service.deleteItem(2);
+        /*service.deleteItem(2);
         List<ItemShop> items = service.getAllItem();
-        for (ItemShop temp: items) {
+        for (ItemShop temp : items) {
             System.out.println(temp.toString());
-        }
+        }*/
     }
 
+
+    public static void MainMenu() throws SQLException, ClassNotFoundException {
+        System.out.println("Choose one of the following: \n" +
+                "PRESS [1] TO ADD ITEMS\n" +
+                "PRESS [2] TO LIST ITEMS\n" +
+                "PRESS [3] TO DELETE ITEMS\n" +
+                "PRESS [0] TO EXIT");
+        choose = keyboard.nextInt();
+
+        if (choose == 1) {// 1. Add Item
+            Scanner in = new Scanner(System.in);
+            System.out.print("Insert name: ");
+            String name = in.toString();
+            System.getProperty("line.separator");
+            double price = in.nextDouble();
+            System.out.print("Insert price: ");
+            item.setName(name);
+            item.setPrice(price);
+            service.addItem(item);
+            System.out.println("Ur item created successfully");
+            MainMenu();
+            return;
+        }
+        if (choose == 2) {// 2. To List Item
+            List<ItemShop> items = service.getAllItem();
+            for (ItemShop temp : items) {
+                System.out.println(temp.toString());
+            }
+            MainMenu();
+            return;
+        }
+        if (choose == 3) {// 2. To Delete Item
+            Scanner in = new Scanner(System.in);
+            System.out.print("Insert ID: ");
+            int id = in.nextInt();
+            service.deleteItem(id);
+            System.out.println("Ur item deleted successfully");
+            MainMenu();
+            return;
+        }
+        if (choose == 0) {
+            System.out.println("You are logged out.");
+            System.exit(0);
+            if (choose > 3) {
+                System.out.println("System ERROR. We  don't have this number operation");
+            }
+            MainMenu();
+        }
+    }
 }
